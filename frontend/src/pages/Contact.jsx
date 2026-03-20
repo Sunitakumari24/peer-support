9a11cd5003d2d8a7c7cf73d8965765cad163f526
Sunitakumari24 <12skumari@gmail.com>
2026-03-20 10:17:42 +0530

    Add Contact page

---
+++ frontend/src/pages/Contact.jsx
@@ -0,0 +1,7 @@
+export default function Contact() {
+	return (
+		<div className="min-h-screen flex items-center justify-center">
+			<p className="text-2xl text-slate-600">Contact Page - Coming Soon</p>
+		</div>
+	)
+}
